115de27f645999424a718e8f0cb40d7b40a63dad
Kokiben <bennassarkaoutar@gmail.com>
2024-04-20 15:04:48 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import shlex
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -8,9 +16,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        cls_dict = {}
+        if cls:
+            dictionar = self.__objects
+            for ky in dictionar:
+                parti = ky.replace('.', ' ')
+                parti = shlex.split(parti)
+                if (parti[0] == cls.__name__):
+                    cls_dict[ky] = self.__objects[ky]
+            return (cls_dict)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +66,13 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+     def delete(self, obj=None):
+        """Delete obj from __objects"""
+        if obj:
+            ky = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[ky]
+
+    def close(self):
+        """Call the reload"""
+        self.reload()
