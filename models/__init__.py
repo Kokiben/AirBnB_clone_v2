5e76a75241019bc545ebb5233ea14393be80eb8a
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-21 02:52:49 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,12 @@ from models.base_model import BaseModel
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 from os import getenv
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 #Add a conditional depending of value of environment variable
