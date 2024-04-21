be30c27a00e3e4c7fb672256195d0f807e352ff7
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-21 01:55:05 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-
+from models.place import plc_amty
 
 class Amenity(BaseModel):
     """class attributes."""
