787170605eee76f89a90da897221f9840ad25425
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-21 02:11:10 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Define the User class."""
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
