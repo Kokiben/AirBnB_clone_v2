aff4c0f5ba1b71c252dffc3e2f5854fb2790dd88
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-21 01:37:57 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
