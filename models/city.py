15cb39a64368dc1ec67596d9dc168ce2ec005b73
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-21 01:21:21 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class City(BaseModel):
     """Class attributes."""
-     __tablename__ = "cities"
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
