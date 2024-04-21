738fd1542bdec6a19ee00008a7614a55911dfdbf
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-04-21 01:57:31 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Review(BaseModel, Base):
     """class attributes."""
-     __tablename__ = "reviews"
+    __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
