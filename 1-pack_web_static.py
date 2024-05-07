5894329f1ea10c502ea0f864f96a003f3b76123c
Kokiben <bennassarkaoutar@gmail.com>
2024-05-06 17:35:18 -0700

    pack web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Print a Fabric script for generates a .tgz"""
+'''Fabric script for generates a .tgz'''
 
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """the web_static folder of your AirBnB Clone repo"""
+    '''The web_static folder of your AirBnB Clone repo'''
     local("mkdir -p versions")
     ph = ("versions/web_static_{}.tgz"
             .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
     rst = local("tar -cvzf {} web_static"
-                   .format(path))
+                   .format(ph))
 
     if rst.failed:
         return None
